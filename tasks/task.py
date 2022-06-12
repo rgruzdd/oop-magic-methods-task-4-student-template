40c03afd6b8b7b87dd97fcee75c6c4abc079963e
Дмитрий Колб <dm.kolb@gmail.com>
2022-06-12 13:36:06 +0000

    Update README.md, tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,7 +1,19 @@
-from typing import List
-
-def check(lst: List):
+class PriceControl:
     """
-    Add your code here or call it from here   
+    Descriptor which don't allow to set price
+    less than 0 and more than 100 included.
     """
     pass
+
+
+class NameControl:
+    """
+    Descriptor which don't allow to change field value after initialization.
+    """
+    pass
+
+
+class Book:
+    author = NameControl()
+    name = NameControl()
+    price = PriceControl()
