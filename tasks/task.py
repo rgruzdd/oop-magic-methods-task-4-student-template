d54d2c2549e094cf51570ee362b8b80c4f157ce2
Gruzin <gruzd.roman@inbox.ru>
2023-04-27 20:14:41 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,19 +1,57 @@
 class PriceControl:
-    """
-    Descriptor which don't allow to set price
-    less than 0 and more than 100 included.
-    """
-    pass
+    def __get__(self, instance, owner):
+        return getattr(instance, self.var_name)
 
+    def __set__(self, instance, value):
+        if value < 0 or value > 100:
+            raise ValueError('Price must be between 0 and 100.')
+        return setattr(instance, self.var_name, value)
+
+    def __set_name__(self, owner, name):
+        self.var_name = "_" + name
 
 class NameControl:
-    """
-    Descriptor which don't allow to change field value after initialization.
-    """
-    pass
+    def __get__(self, instance, owner):
+        return getattr(instance, self.var_name)
 
+    def __set__(self, instance, value):
+        new_value = instance.__dict__[self.var_name]
+        if new_value == '':
+            return setattr(instance, self.var_name, value)
+        elif new_value != value:
+            if self.var_name == '_name':
+                raise ValueError('Name can not be changed')
+            elif self.var_name == '_author':
+                raise ValueError('Author can not be changed')
+        return setattr(instance, self.var_name, value)
+
+    def __set_name__(self, owner, name):
+        self.var_name = "_" + name
+
+    #     global a
+    #     a = value
+    #     if a != value:
+    #         if self.var_name == '_name':
+    #             raise ValueError('Name can not be changed')
+    #         elif self.var_name == '_author':
+    #             raise ValueError('Author can not be changed')
+    #     return setattr(instance, self.var_name, value)
+    #
+    # def __set_name__(self, owner, name):
+    #     self.var_name = "_" + name
 
 class Book:
     author = NameControl()
     name = NameControl()
     price = PriceControl()
+    def __init__(self, author, name, price):
+        self._author = author
+        self._name = name
+        self.price = price
+
+b = Book("William Faulkner", "The Sound and the Fury", 12)
+
+b.author = " Roma Big Dick 2000"
+
+
+
